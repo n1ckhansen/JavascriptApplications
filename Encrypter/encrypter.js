bbfaed53ec678d56f21d78fea2896f403cc07b89
Nick Hansen <nhansen@blackfez.com>
2017-06-12 19:28:22 -0500

    Encrypt with salt works.  Decrypt not so much.  The simple bit flip didn't work as expected.

--- Encrypter/encrypter.js
+++ Encrypter/encrypter.js
@@ -1,4 +1,5 @@
 var ALPHABET = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ];
+var den = 1;
 
 function rot13Encrpyt( rt ) {
 	enc13 = "";
@@ -9,15 +10,36 @@ function rot13Encrpyt( rt ) {
 	return enc13;
 }
 
+function saltEncrypt( salt, raw ) {
+	var encrypted = "";
+	var saltIndex = -1;
+	for( i = 0; i<raw.length; i++ ) {
+		var theLetter = raw[ i ];
+		console.log( "before saltIndex is " + saltIndex );
+		saltIndex++
+		if( saltIndex == salt.length ) {
+			saltIndex = 0;
+		}
+		console.log( "after saltIndex is " + saltIndex );
+		var theSaltChar = salt[ saltIndex ].toUpperCase();
+		var theSaltRot = ALPHABET.indexOf( theSaltChar );
+		encrypted += rotCrypt( theSaltRot, theLetter );
+		console.log( encrypted );
+	}
+	return encrypted;
+}
+
 function rotCrypt( rot, raw ) {
 	var thisLetter = raw.toUpperCase();
 	var thisIndex = ALPHABET.indexOf( thisLetter );
+	rot = rot * den;
 	if( thisIndex < 0 ) {
 		console.log( thisLetter );
 		return thisLetter;
 	}
 	else {
 		var newIndex = ( thisIndex + rot ) % 26;
+		console.log( "newIndex is " + newIndex );
 		console.log( ALPHABET[ newIndex ] );
 		return ALPHABET[ newIndex ];
 	}
@@ -31,8 +53,12 @@ function setUp() {
 
 function processEncryptForm() {
 	var inText = document.getElementById( 'rawText' ).value;
+	var inSalt = document.getElementById( 'saltText' ).value;
+	den = 1;
 	console.log( "inText is " + inText );
-	var encrypted = rot13Encrpyt( inText );
+	console.log( "saltText is " + inSalt );
+	//var encrypted = rot13Encrpyt( inText );
+	var encrypted = saltEncrypt( inSalt, inText );
 	console.log( encrypted );
 	document.getElementById( "encryptedText" ).value = encrypted;
 	printResult( encrypted);
@@ -40,11 +66,14 @@ function processEncryptForm() {
 
 function processDecryptForm() {
 	var inText = document.getElementById( 'encryptedText' ).value;
+	var inSalt = document.getElementById( 'saltText' ).value;
+	den = -1;
 	console.log( "inText is " + inText );
-	var encrypted = rot13Encrpyt( inText );
-	console.log( encrypted );
-	document.getElementById( "rawText" ).value = encrypted;
-	printResult( encrypted);
+	//var encrypted = rot13Encrpyt( inText );
+	var dencrypted = saltEncrypt( inSalt, inText );
+	console.log( dencrypted );
+	document.getElementById( "rawText" ).value = dencrypted;
+	printResult( dencrypted);
 }
 
 function printResult( rText ) {
